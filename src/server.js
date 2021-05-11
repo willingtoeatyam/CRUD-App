b4383703705dfba34c581302896d0d81178f0586
William Imoukhuede <wimoukhuede@gmail.com>
2021-05-11 15:19:25 +0100

    Removed Port

--- src/server.js
+++ src/server.js
@@ -11,4 +11,4 @@ dbSetup();
 app.use(express.json());
 app.use(detailRoutes);
 
-app.listen(port || 4000, ()=> console.log(`Server is listening on port ${port}`));
+app.listen(port , ()=> console.log(`Server is listening on port ${port}`));
